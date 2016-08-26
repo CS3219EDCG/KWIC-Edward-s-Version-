9a8ec06862dbbb49f7792fcc47c39f6f0e560650
Edward <angertrail@hotmail.com>
2016-08-26 18:51:38 +0800

    Initialise the storage class and added 2 functions to split lines

--- src/kwic/Storage.java
+++ src/kwic/Storage.java
@@ -1,10 +1,40 @@
 package kwic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/* This is the core storage of the entire program */
+
 public class Storage {
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    private ArrayList<String> lines;
+    private ArrayList<ArrayList<String>> words;
+    
+    public Storage() {
+        this.lines = new ArrayList<String>();
+        this.words = new ArrayList<ArrayList<String>>();        
+    }
+    
+    public Storage(ArrayList<String> lines) {
+        this.lines = lines;
+        this.words = splitLines(lines);
+    }
+    
+    //This function splits up all the lines in the arraylist into their own array of words
+    private ArrayList<ArrayList<String>> splitLines(ArrayList<String> lines) {
+        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+        
+        for (int i=0; i< lines.size(); i++) {
+            result.add(splitLine(lines.get(i)));
+        }
+        
+        return result;
+        
+    }
+    
+    
+    //This function splits up the particular line into an array of word
+    private ArrayList<String> splitLine(String line) {
+        return new ArrayList<String>(Arrays.asList(line.split(" ")));
     }
 
 }
