0d7a0953bed928d1b216797341317da045a884c8
Edward <angertrail@hotmail.com>
2016-08-26 22:22:14 +0800

    Initialise class and create function to set Cases

--- src/kwic/CircularShift.java
+++ src/kwic/CircularShift.java
@@ -1,9 +1,42 @@
 package kwic;
 
-public class CircularShift {
+import java.util.ArrayList;
+import java.util.Collections;
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+public class CircularShift {
+    
+    private Storage storage;
+    private ArrayList<String> ignoreList;
+    
+    public CircularShift(Storage storage, ArrayList<String> ignoreList) {
+        this.storage = storage;
+        this.ignoreList = ignoreList;
+    }
+    
+    private void caseSet(ArrayList<ArrayList<String>> words) {
+        for (int i=0; i<words.size(); i++) {
+            for (int j=0; j< words.get(i).size(); j++) {
+                String word = words.get(i).get(j);
+                if (inIgnoreList(word)) {
+                    String iWord = word.toLowerCase();
+                    words.get(i).set(j, iWord);
+                } else {
+                    String nWord = word.substring(0, 1).toUpperCase() + word.substring(1);
+                    words.get(i).set(j, nWord);
+                }
+                
+            }
+        }
+    }
+    
+    private boolean inIgnoreList(String word) {
+        for (int i=0; i<ignoreList.size(); i++) {
+            if (ignoreList.get(i).toLowerCase().equals(word)) {
+                return true;
+            }
+        }
+        
+        return false;
 
     }
 
