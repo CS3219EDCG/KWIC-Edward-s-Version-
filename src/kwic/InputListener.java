bd15da825a3984e0dde05fcec96e33a39279725c
Edward <angertrail@hotmail.com>
2016-08-27 22:18:06 +0800

    Completed InputListener class

---
+++ src/kwic/InputListener.java
@@ -0,0 +1,10 @@
+package kwic;
+
+public interface InputListener {
+    
+    public void handleLineAddedEvent(String line);
+
+    public void handleIgnoreWordAddedEvent(String word);
+
+    public void handleInputFinishedEvent();
+}
