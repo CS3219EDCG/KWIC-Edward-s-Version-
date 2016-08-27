b8fe5ac66ad080c1702b27fd0e6f85e8c49df2b6
Edward <angertrail@hotmail.com>
2016-08-27 22:09:17 +0800

    Completed alphabetizer function

--- src/kwic/Alphabetizer.java
+++ src/kwic/Alphabetizer.java
@@ -1,10 +1,29 @@
 package kwic;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Alphabetizer {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    private CircularShift circularShift;
+    private ArrayList<String> lines;
+    
+    public Alphabetizer(CircularShift circularShift) {
+        this.circularShift = circularShift;
+        this.lines = this.alphabetizer(circularShift.getShiftedList());
     }
+        
+    class arrListComparator implements Comparator<String> {
 
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
+        }
+        
+    }
+    
+    private ArrayList<String> alphabetizer(ArrayList<String> rLines) {
+        Collections.sort(rLines, new arrListComparator());
+        return rLines;
+    }
+    
 }
