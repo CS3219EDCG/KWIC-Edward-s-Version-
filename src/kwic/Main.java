e577a4d23ea58cf754a1a38f0c81393c6490c826
Edward <angertrail@hotmail.com>
2016-08-27 22:34:02 +0800

    Completed main class

--- src/kwic/Main.java
+++ src/kwic/Main.java
@@ -3,8 +3,9 @@ package kwic;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        MasterControl masterControl = new MasterControl();
+        
+        masterControl.start();
     }
 
 }
